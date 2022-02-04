67c4ac2d9a0421fed55b11d4dfc6f5eb02f64f59
Jeff Dinkelman <mfankenstein@yahoo.com>
2022-02-04 10:36:11 -0800

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,49 @@
+import '@nomiclabs/hardhat-ethers';
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import '@typechain/hardhat';
+// import "hardhat-gas-reporter";
+// import "@nomiclabs/hardhat-etherscan";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+         version: '0.8.0',
+      },
+    ]
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ['local']
+    },
+    hardhat: {
+      live: false,
+      saveDeployments: true,
+      tags: ['local', 'test'],
+    }
+    // hardhat: {
+    //   live: false,
+    //   saveDeployments: true,
+    //   tags: ['local', 'test'],
+    //   forking: {
+    //     url: process.env.ALCHEMY_MAINNET_RPC_URL!,
+    //     blockNumber: 13910565
+    //   }
+    // }
+  },
+  typechain: {
+    outDir: "./typechain-types",
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: true
+  },
+
+};
+export default config;
