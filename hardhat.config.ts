184951175dd29d9fbff88ded76084c67b62ca6bd
Jeff Dinkelman <mfankenstein@yahoo.com>
2022-03-17 22:20:21 -0700

    adds packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import 'hardhat-deploy';
 import '@typechain/hardhat';
 // import "hardhat-gas-reporter";
 // import "@nomiclabs/hardhat-etherscan";
+// import "hardhat-contract-sizer";
+import 'solidity-coverage'; // npx hardhat coverage [command-options]
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -16,7 +18,11 @@ const config: HardhatUserConfig = {
     ]
   },
   namedAccounts: {
-    deployer: 0,
+    deployer: {
+      default: 0,
+      // "rinkeby": process.env.<ENTER_DEPLOYER>!
+    },
+    user1: 1,
   },
   networks: {
     localhost: {
@@ -28,7 +34,7 @@ const config: HardhatUserConfig = {
       live: false,
       saveDeployments: true,
       tags: ['local', 'test'],
-    }
+    },
     // hardhat: {
     //   live: false,
     //   saveDeployments: true,
@@ -37,7 +43,21 @@ const config: HardhatUserConfig = {
     //     url: process.env.ALCHEMY_MAINNET_RPC_URL!,
     //     blockNumber: 13910565
     //   }
-    // }
+    // },
+    // rinkeby: {
+    //   url: process.env.RINKEBY_URL || "",
+    //   accounts:
+    //     process.env.<ENTER_PRIVATE_KEY> !== undefined ?
+    //       [process.env.<ENTER_PRIVATE_KEY>] : [],
+    //   tags: ['rinkeby']
+    // },
+    // goerli: {
+    //   url: process.env.GOERLI_URL || "",
+    //   accounts:
+    //     process.env.<ENTER_PRIVATE_KEY> !== undefined ?
+    //       [process.env.<ENTER_PRIVATE_KEY>] : [],
+    //   tags: ['goerli']
+    // },
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY
@@ -47,6 +67,10 @@ const config: HardhatUserConfig = {
     target: 'ethers-v5',
     alwaysGenerateOverloads: true
   },
-
+  // contractSizer: {    // npx hardhat size-contracts
+  //   alphaSort: true,
+  //   disambiguatePaths: false,
+  //   runOnCompile: true,
+  // },
 };
 export default config;
--- test/basicNFT.ts
+++ test/basicNFT.ts
@@ -2,17 +2,26 @@ import {expect} from "./chai-setup";
 import {ethers, deployments, getNamedAccounts} from 'hardhat';
 import { Deployment } from "hardhat-deploy/dist/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+// import { MockContract, MockContractFactory, smock } from '@defi-wonderland/smock';
 import {setup} from './utils/index';
 import type { BasicNFT } from '../typechain-types';
+// import type { BasicNFT, BasicNFT__factory } from '../typechain-types';
+
 
 describe('BasicNFT', () => {
   let deployer: SignerWithAddress;
   let basicNFT: BasicNFT;
 
+  // let basicNFT: MockContract<BasicNFT>;
+  // let basicNFTFactory: MockContractFactory<BasicNFT__factory>;
+
   beforeEach(async () => {
     ({deployer} = await setup());
 
     await deployments.fixture(['basicNFT']);
+
+    // basicNFTFactory = await smock.mock<BasicNFT__factory>('BasicNFT');
+    // basicNFT = await basicNFTFactory.deploy();
 
     const BasicNFTDeployment: Deployment = await deployments.get('BasicNFT');
     basicNFT = await Promise.resolve(ethers.getContractAt('BasicNFT', BasicNFTDeployment.address) as Promise<BasicNFT>);
